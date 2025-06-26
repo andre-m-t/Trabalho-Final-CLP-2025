93fbb4bb17871028373bf89f43e484d7579816bc
andre-m-t <andre_moreira.9@hotmail.com>
2025-06-26 00:57:12 -0300

    Alteração de nomes de alguns assets

---
+++ CLP_ListadeInstrucoes/src/Telas/PainelComImagem.java
@@ -0,0 +1,24 @@
+package Telas;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class PainelComImagem extends JPanel {
+
+    private Image imagem;
+
+    public PainelComImagem() {
+        // Substitua esse caminho pelo caminho real da sua imagem na pasta src
+        imagem = new ImageIcon(getClass().getResource("/Assets/EntortasBarSimulation/Background.png")).getImage();
+        setOpaque(false);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        // Desenha a imagem preenchendo o tamanho do painel
+        g.drawImage(imagem, 0, 0, getWidth(), getHeight(), this);
+    }
+}
